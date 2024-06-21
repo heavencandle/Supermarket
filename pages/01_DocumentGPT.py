2b745578d1899538f6cda6ec516e096dabaf078b
heavencandle <whj0210@gmail.com>
2024-06-21 21:39:38 +0900

    Refactor and add caching

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -9,32 +9,21 @@ from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
 import streamlit as st
 
 st.set_page_config(
-    page_title="DocumentGPT",
+    page_title="DocumentDetective",
     page_icon="📃",
 )
 
-st.title("DocumentGPT")
-
-st.markdown("""
-Welcome!
-
-Use this chatbot to ask questions to Mangle the data detective about your files!
-""")
-
-file = st.file_uploader("Upload a .txt .pdfor .docx file", type=["pdf", "txt", "docx"])
-
-if file:
-    st.write(file)
+def embed_file(file):
     file_content = file.read()
     file_path = f"./.cache/files/{file.name}"
     with open(file_path, "wb") as f:
         f.write(file_content)
 
-    cache_dir = LocalFileStore("./.cache/embeddings/{file.name}")
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
 
     splitter = CharacterTextSplitter.from_tiktoken_encoder(
-        separator="#",
-        chunk_size=4000, # Should be small enough, but not to distort the meaning or context
+        separator="\n",
+        chunk_size=4000, 
         chunk_overlap=True,
     )
 
@@ -46,55 +35,23 @@ if file:
         embeddings, cache_dir
     )
 
-    vectorstore = FAISS.from_documents(docs, embeddings)
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
 
     retriever = vectorstore.as_retriever()
 
-
-    # map_doc_prompt = ChatPromptTemplate.from_messages(
-    #     [
-    #         ("system",
-    #         """
-    #         Use the following portion of a long document to see if any of the text is relevant to answer the question. 
-    #         Return any relevant text verbatim.
-    #         -------
-    #         {context}
-    #         """
-    #         ),
-    #         ("human", "{question}")
-    #     ]
-    # )
-
-    # map_doc_chain = map_doc_prompt | llm
-
-    # def map_docs(inputs):
-    #     documents = inputs['documents']
-    #     question = inputs['question']
-
-    #     return "\n\n".join(
-    #         map_doc_chain.invoke(
-    #             {"context": doc.page_content, "question": question}
-    #         ).content 
-    #         for doc in documents
-    #     )
+    return retriever
 
 
-    # map_chain = { "documents": retriever, "question": RunnablePassthrough() } | RunnableLambda(map_docs)
+st.title("DocumentDetective")
 
+st.markdown("""
+Welcome!
 
-    # final_prompt = ChatPromptTemplate.from_messages([
-    #     (
-    #         "system", 
-    #         """
-    #         Given the following extracted parts of a long document and a question,
-    #         create a final answer.
-    #         I you don't know the answer, just say that you don't know. 
-    #         Dont try to make up an answer.
-    #         ------
-    #         {context}
-    #         """
-    #     ), 
-    #     ("human", "{question}")
-    # ])
+Use this chatbot to ask questions to Mangle the detective about your files!
+""")
 
-    # chain = { "context": map_chain, "question": RunnablePassthrough()} | final_prompt | llm
+file = st.file_uploader("Upload a .txt .pdf or .docx file", type=["pdf", "txt", "docx"])
+
+if file:
+    retriever = embed_file(file)
+    t = retriever.invoke("test")
