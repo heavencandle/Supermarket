6e0ecd7df974e03aa6ddd2aff3324d403c47e148
heavencandle <whj0210@gmail.com>
2024-07-10 01:20:45 +0900

    Fix prompt and add generate quiz button

--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -3,6 +3,8 @@ from langchain.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import CharacterTextSplitter
 import streamlit as st
 from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.callbacks import StreamingStdOutCallbackHandler
 
 
 st.set_page_config(
@@ -28,9 +30,16 @@ def split_file(file):
 
     return docs
 
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
 llm = ChatOpenAI(
     temperature=0.1,
-    model="gpt-3.5-turbo-1106"
+    model="gpt-3.5-turbo-1106",
+    streaming=True,
+    callbacks=[
+        StreamingStdOutCallbackHandler()
+    ]
 )
 
 with st.container():
@@ -61,5 +70,51 @@ with st.container():
     """)
         
     else:
-        st.write(docs)
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                (
+                    "system",
+                    """
+You are a helpful assistant that is role playing as a teacher.
+
+Based ONLY on the following context make 10 questions to test the user's knowledge about the text.
+
+Each question should have 4 answers, three of them must be incorrect and one should be correct.
+
+Use (o) to signal the correct answer.
+
+Question examples:
+
+Question: What is the color of the ocean?
+Answers: Red|Yellow|Green|Blue(o)
+
+Question: What is the capital of Georgia?
+Answers: Baku|Tbilisi(o)|Manila|Beirut
+
+Question: When was Avatar released?
+Answers: 2007|2001|2009(o)|1998
+
+Question: Who was Julius Caesar?
+Answer: A Roman Emperor(o)|Painter|Actor|Model
+
+Your turn!
+
+Context: {context}
+
+"""
+                )
+            ]
+
+        )
+        
+        chain = prompt{
+            "context": format_docs
+        } | llm
+        
+        start = st.button("Generate Quiz")
+
+        if start:
+            chain.invoke(docs)
+
+
     
