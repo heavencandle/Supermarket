b6fcfc071944a928c1c58e5dc2c6df5c794c9057
heavencandle <whj0210@gmail.com>
2024-07-10 23:55:22 +0900

    Add formatting llm

--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -12,7 +12,6 @@ st.set_page_config(
     page_icon="❓",
 )
 
-
 @st.cache_data(show_spinner="Loading file...")
 def split_file(file):
     file_content = file.read()
@@ -42,6 +41,168 @@ llm = ChatOpenAI(
     ]
 )
 
+questions_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+You are a helpful assistant that is role playing as a teacher.
+
+Based ONLY on the following context make 10 questions to test the user's knowledge about the text.
+
+Each question should have 4 answers, three of them must be incorrect and one should be correct.
+
+Use (o) to signal the correct answer.
+
+Question examples:
+
+Question: What is the color of the ocean?
+Answers: Red|Yellow|Green|Blue(o)
+
+Question: What is the capital of Georgia?
+Answers: Baku|Tbilisi(o)|Manila|Beirut
+
+Question: When was Avatar released?
+Answers: 2007|2001|2009(o)|1998
+
+Question: Who was Julius Caesar?
+Answer: A Roman Emperor(o)|Painter|Actor|Model
+
+Your turn!
+
+Context: {context}
+
+"""
+        )
+    ]
+
+)
+
+questions_chain = {
+    "context": format_docs
+    } |questions_prompt | llm
+
+formatting_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+You are a powerful formatting algorithm.
+     
+    You format exam questions into JSON format.
+    Answers with (o) are the correct ones.
+     
+    Example Input:
+    Question: What is the color of the ocean?
+    Answers: Red|Yellow|Green|Blue(o)
+         
+    Question: What is the capital or Georgia?
+    Answers: Baku|Tbilisi(o)|Manila|Beirut
+         
+    Question: When was Avatar released?
+    Answers: 2007|2001|2009(o)|1998
+         
+    Question: Who was Julius Caesar?
+    Answers: A Roman Emperor(o)|Painter|Actor|Model
+    
+     
+    Example Output:
+     
+    ```json
+    {{ "questions": [
+            {{
+                "question": "What is the color of the ocean?",
+                "answers": [
+                        {{
+                            "answer": "Red",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Yellow",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Green",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Blue",
+                            "correct": true
+                        }},
+                ]
+            }},
+                        {{
+                "question": "What is the capital or Georgia?",
+                "answers": [
+                        {{
+                            "answer": "Baku",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Tbilisi",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "Manila",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Beirut",
+                            "correct": false
+                        }},
+                ]
+            }},
+                        {{
+                "question": "When was Avatar released?",
+                "answers": [
+                        {{
+                            "answer": "2007",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "2001",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "2009",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "1998",
+                            "correct": false
+                        }},
+                ]
+            }},
+            {{
+                "question": "Who was Julius Caesar?",
+                "answers": [
+                        {{
+                            "answer": "A Roman Emperor",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "Painter",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Actor",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Model",
+                            "correct": false
+                        }},
+                ]
+            }}
+        ]
+     }}
+"""
+        )
+    ]
+)
+
+formatting_chain = formatting_prompt | llm
+
 with st.container():
     docs = None
     choice = st.selectbox("Choose what you want to use.",
@@ -70,51 +231,14 @@ with st.container():
     """)
         
     else:
-        prompt = ChatPromptTemplate.from_messages(
-            [
-                (
-                    "system",
-                    """
-You are a helpful assistant that is role playing as a teacher.
 
-Based ONLY on the following context make 10 questions to test the user's knowledge about the text.
-
-Each question should have 4 answers, three of them must be incorrect and one should be correct.
-
-Use (o) to signal the correct answer.
-
-Question examples:
-
-Question: What is the color of the ocean?
-Answers: Red|Yellow|Green|Blue(o)
-
-Question: What is the capital of Georgia?
-Answers: Baku|Tbilisi(o)|Manila|Beirut
-
-Question: When was Avatar released?
-Answers: 2007|2001|2009(o)|1998
-
-Question: Who was Julius Caesar?
-Answer: A Roman Emperor(o)|Painter|Actor|Model
-
-Your turn!
-
-Context: {context}
-
-"""
-                )
-            ]
-
-        )
-        
-        chain = prompt{
-            "context": format_docs
-        } | llm
-        
         start = st.button("Generate Quiz")
 
         if start:
-            chain.invoke(docs)
+            questions_response = questions_chain.invoke(docs)
+            formatting_chain({
+                "context": questions_response.content
+            })
 
 
     
