32df8f8612b21a05445b35e810f4202a6ef62a36
heavencandle <62787552+heavencandle@users.noreply.github.com>
2024-07-24 18:33:42 +0900

    Add video file uploader

--- pages/05_MeetingGPT.py
+++ pages/05_MeetingGPT.py
@@ -1,6 +1,38 @@
 import streamlit as st
+import subprocess
+import math
+from pydub import AudioSegment
+import glob
+import openai
+import os
 
 st.set_page_config(
     page_title="MeetingGPT",
     page_icon="💼",
 )
+
+
+st.markdown(
+    """
+# MeetingGPT
+            
+Welcome to MeetingGPT, upload a video and I will give you a transcript, a summary and a chat bot to ask any questions about it.
+Get started by uploading a video file in the sidebar.
+"""
+)
+
+with st.sidebar:
+    video = st.file_uploader(
+        "Video",
+        type=["mp4", "avi", "mkv", "mov"],
+    )
+
+if video:
+    chunks_folder = "./.cache/chunks"
+    with st.status("Loading video..."):
+        video_content = video.read()
+        video_path = f"./.cache/{video.name}"
+        audio_path = video_path.replace("mp4", "mp3")
+        transcript_path = video_path.replace("mp4", "txt")
+        with open(video_path, "wb") as f:
+            f.write(video_content)
