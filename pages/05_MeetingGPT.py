94424b811b714094cc026d33d85b0f93977d5f8b
heavencandle <whj0210@gmail.com>
2024-07-28 15:29:30 +0900

    Add extract and transcribe method

--- pages/05_MeetingGPT.py
+++ pages/05_MeetingGPT.py
@@ -21,18 +21,49 @@ Get started by uploading a video file in the sidebar.
 """
 )
 
-with st.sidebar:
+def extract_audio_from_video(video_path):
+    command = [
+        "ffmpeg",
+        "-i",
+        video_path,
+        "-vn",
+        video_path.replace("mp4", "mp3")
+    ]
+    subprocess.run(command)
+
+def transcribe_chunks(chunk_folder, destination):
+    files = glob.glob(f"{chunk_folder}/*.mp3")
+    final_transcript = ""
+
+    for file in files:
+        with open(file, "rb") as audio_file:
+            transcript = openai.Audio.transcribe("whisper-1", audio_file)
+            final_transcript += transcript["text"]
+    
+    with open(destination, "w") as file:
+        file.write(final_transcript)
+
+with st.container():
     video = st.file_uploader(
         "Video",
         type=["mp4", "avi", "mkv", "mov"],
     )
 
-if video:
-    chunks_folder = "./.cache/chunks"
-    with st.status("Loading video..."):
+with st.container():
+    if video:
         video_content = video.read()
-        video_path = f"./.cache/{video.name}"
-        audio_path = video_path.replace("mp4", "mp3")
-        transcript_path = video_path.replace("mp4", "txt")
+        video_path = f"./cache/files/{video.name}"
         with open(video_path, "wb") as f:
             f.write(video_content)
+        
+        extract_audio_from_video(video_path)
+
+
+        chunks_folder = "./.cache/chunks"
+        with st.status("Loading video..."):
+            video_content = video.read()
+            video_path = f"./.cache/{video.name}"
+            audio_path = video_path.replace("mp4", "mp3")
+            transcript_path = video_path.replace("mp4", "txt")
+            with open(video_path, "wb") as f:
+                f.write(video_content)
