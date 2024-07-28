5bef80d3c3db8c288a69f523ba8a2a17ebb50d26
heavencandle <whj0210@gmail.com>
2024-07-29 00:04:16 +0900

    Add caching

--- pages/05_MeetingGPT.py
+++ pages/05_MeetingGPT.py
@@ -6,6 +6,60 @@ import glob
 import openai
 import os
 
+# only for dev
+has_transcript = os.path.exists("./.cache/podcast.txt")
+
+@st.cache_data()
+def extract_audio_from_video(video_path):
+    if has_transcript:
+        return
+    
+    command = [
+        "ffmpeg",
+        "-y",
+        "-i",
+        video_path,
+        "-vn",
+        video_path.replace("mp4", "mp3")
+    ]
+    subprocess.run(command)
+
+@st.cache_data()
+def cut_audio_in_chunks(audio_path, chunk_size, chunks_folder):
+    if has_transcript:
+        return
+    
+    track = AudioSegment.from_mp3(audio_path)
+    # track.duration_seconds
+
+    chunk_len = chunk_size * 60 * 1000
+    chunks = math.ceil(len(track) / chunk_len)
+
+    for i in range(chunks):
+        start_time = i * chunk_len
+        end_time = (i + 1) * chunk_len
+
+        chunk = track[start_time:end_time]
+
+        chunk.export(f"{chunks_folder}/chunk_{i}.mp3", format="mp3")
+
+@st.cache_data()
+def transcribe_chunks(chunk_folder, destination):
+    if has_transcript:
+        return
+
+    files = glob.glob(f"{chunk_folder}/*.mp3")
+    files.sort()
+    final_transcript = ""
+
+    for file in files:
+        with open(file, "rb") as audio_file:
+            transcript = openai.Audio.transcribe("whisper-1", audio_file)
+            final_transcript += transcript["text"]
+    
+    with open(destination, "w") as file:
+        file.write(final_transcript)
+
 st.set_page_config(
     page_title="MeetingGPT",
     page_icon="💼",
@@ -21,28 +75,6 @@ Get started by uploading a video file in the sidebar.
 """
 )
 
-def extract_audio_from_video(video_path):
-    command = [
-        "ffmpeg",
-        "-i",
-        video_path,
-        "-vn",
-        video_path.replace("mp4", "mp3")
-    ]
-    subprocess.run(command)
-
-def transcribe_chunks(chunk_folder, destination):
-    files = glob.glob(f"{chunk_folder}/*.mp3")
-    final_transcript = ""
-
-    for file in files:
-        with open(file, "rb") as audio_file:
-            transcript = openai.Audio.transcribe("whisper-1", audio_file)
-            final_transcript += transcript["text"]
-    
-    with open(destination, "w") as file:
-        file.write(final_transcript)
-
 with st.container():
     video = st.file_uploader(
         "Video",
@@ -50,20 +82,19 @@ with st.container():
     )
 
 with st.container():
-    if video:
-        video_content = video.read()
-        video_path = f"./cache/files/{video.name}"
-        with open(video_path, "wb") as f:
-            f.write(video_content)
-        
-        extract_audio_from_video(video_path)
-
-
+    if video:        
         chunks_folder = "./.cache/chunks"
+
         with st.status("Loading video..."):
             video_content = video.read()
             video_path = f"./.cache/{video.name}"
-            audio_path = video_path.replace("mp4", "mp3")
+            audio_path = video_path.replace("mp4", "mp3")    
             transcript_path = video_path.replace("mp4", "txt")
             with open(video_path, "wb") as f:
                 f.write(video_content)
+        with st.status("Extracting audio..."):
+            extract_audio_from_video(video_path)
+        with st.status("Cutting audio segments..."):
+            cut_audio_in_chunks(audio_path, 10, chunks_folder)
+        with st.status("Transcribing audio..."):
+            transcribe_chunks(chunks_folder, transcript_path)
